92c1370b66ce164466a0058cc6e68a85970b8c28
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-28 15:40:00 -0600

    linked to correct css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 		<!-- custom CSS stylesheet -->
-		<link rel="stylesheet" href="css/style.css" type="text/css" />
+		<link rel="stylesheet" href="style-answers.css" type="text/css" />
 
 		<title>CSS Mockup Activity</title>
 	</head>
